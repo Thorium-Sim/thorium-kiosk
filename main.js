c78c0d7fcc4f9d6fafdc067467c8f251c6e992c4
Alex Anderson <alexanderson1993@gmail.com>
2017-05-20 09:52:39 -0600

    Add node notifier, set the default password.

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const electron = require('electron')
 // Module to control application life.
+const NotificationCenter = require('node-notifier').NotificationCenter;
+const password = 'rommel1942';
+
+var notifier = new NotificationCenter({
+  withFallback: false, // Use Growl Fallback if <= 10.8
+  customPath: void 0 // Relative/Absolute path to binary if you want to use your own fork of terminal-notifier
+});
+
 const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
@@ -24,7 +32,26 @@ function createWindow () {
 
   globalShortcut.register('CommandOrControl+Alt+E', function () {
     // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    notifier.notify({
+      'title': "Access Dev Tools",
+      'subtitle': "Access Restricted",
+      'message': 'Click "reply" to enter password.',
+      'sound': false, // Case Sensitive string for location of sound file, or use one of macOS' native sounds (see below)
+      'icon': 'Terminal Icon', // Absolute Path to Triggering Icon
+      'contentImage': void 0, // Absolute Path to Attached Image (Content Image)
+      'open': void 0, // URL to open on Click
+      'wait': false, // Wait for User Action against Notification or times out. Same as timeout = 5 seconds
+
+      // New in latest version. See `example/macInput.js` for usage
+      closeLabel: void 0, // String. Label for cancel button
+      actions: void 0, // String | Array<String>. Action label or list of labels in case of dropdown
+      dropdownLabel: void 0, // String. Label to be used if multiple actions
+      reply: true // Boolean. If notification should take input. Value passed as third argument in callback and event emitter.
+    }, function(error, response, metadata) {
+      if (metadata.activationValue === password){
+        mainWindow.webContents.openDevTools();
+      }
+    });
   });
   
   globalShortcut.register('CommandOrControl+Q', function () {
@@ -41,7 +68,26 @@ function createWindow () {
 
   globalShortcut.register('CommandOrControl+Alt+Q', function(){
     //Actually quit
-    app.quit();
+    notifier.notify({
+      'title': "Quit Application",
+      'subtitle': "Access Restricted",
+      'message': 'Click "reply" to enter password.',
+      'sound': false, // Case Sensitive string for location of sound file, or use one of macOS' native sounds (see below)
+      'icon': 'Terminal Icon', // Absolute Path to Triggering Icon
+      'contentImage': void 0, // Absolute Path to Attached Image (Content Image)
+      'open': void 0, // URL to open on Click
+      'wait': false, // Wait for User Action against Notification or times out. Same as timeout = 5 seconds
+
+      // New in latest version. See `example/macInput.js` for usage
+      closeLabel: void 0, // String. Label for cancel button
+      actions: void 0, // String | Array<String>. Action label or list of labels in case of dropdown
+      dropdownLabel: void 0, // String. Label to be used if multiple actions
+      reply: true // Boolean. If notification should take input. Value passed as third argument in callback and event emitter.
+    }, function(error, response, metadata) {
+      if (metadata.activationValue === password){
+        app.quit();
+      }
+    });
   })
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
